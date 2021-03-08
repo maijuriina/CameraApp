15ba09bbb302b64994c68e86f71b5eb514201c6c
Maijuriina Mustonen <42972494+maijuriina@users.noreply.github.com>
2021-03-08 08:28:39 +0200

    Initial Commit

---
+++ Shared/CameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraAppApp.swift
+//  Shared
+//
+//  Created by Maijuriina Mustonen on 08/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct CameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

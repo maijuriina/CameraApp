cd143d423176575d6dc3e7219e356f64ef4051c9
Maijuriina Mustonen <42972494+maijuriina@users.noreply.github.com>
2021-03-08 10:17:25 +0200

    Added ImagePickerView for accessing gallery and binding image to the one chosen. Multiple functions created for imagePicker isVisible and image. Gallery picking works.

--- Shared/Views/ContentView.swift
+++ Shared/Views/ContentView.swift
@@ -8,35 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showImagePicker:Bool = false
     @State var showActionSheet:Bool = false
+    @State var image: Image?
     
     var body: some View {
-        VStack {
-            Image("cat_photo")
-                .resizable()
-                .clipped()
-                .clipShape(Circle())
-                .aspectRatio(contentMode: .fill)
-                .frame(width: 160, height: 160)
-                .overlay(
-                    CameraButtonView(showActionSheet: $showActionSheet)
-                        .offset(x: 50, y: 65)
-                )
-            Text("Kitty McWhiskers")
-                .fontWeight(.bold)
-                .padding()
-        }.offset(y: -70)
-        .actionSheet(isPresented: $showActionSheet, content: { () -> ActionSheet in ActionSheet(title: Text("Select Image"), message: Text("Please select an image from the image gallery or use the camera"), buttons: [
-        ActionSheet.Button.default(Text("Camera"), action: {
+        ZStack {
+            VStack {
+                image?
+                    .resizable()
+                    .clipped()
+                    .clipShape(Circle())
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 160, height: 160)
+                    .overlay(
+                        CameraButtonView(showActionSheet: $showActionSheet)
+                            .offset(x: 50, y: 65)
+                    )
+                Text("Kitty McWhiskers")
+                    .fontWeight(.bold)
+                    .padding()
+            }.offset(y: -70)
+            .actionSheet(isPresented: $showActionSheet, content: { () -> ActionSheet in ActionSheet(title: Text("Select Image"), message: Text("Please select an image from the image gallery or use the camera"), buttons: [
+                ActionSheet.Button.default(Text("Camera"), action: {
         
-        }), ActionSheet.Button.default(Text("Photo Gallery"), action: {
-        
-        }),
-        ActionSheet.Button.cancel()
-        ])
+                }), ActionSheet.Button.default(Text("Photo Gallery"), action: {
+                    self.showImagePicker.toggle()
+                }),
+                ActionSheet.Button.cancel()
+            ])
         })
-        Spacer()
+        if showImagePicker {
+            ImagePickerView(isVisible: $showImagePicker, image: $image)
+        }
     }
+        .onAppear { self.image = Image("cat_photo") }
+}
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ Shared/Views/ImagePickerView.swift
@@ -0,0 +1,51 @@
+//
+//  ImagePickerView.swift
+//  CameraApp
+//
+//  Created by Maijuriina Mustonen on 08/03/2021.
+//
+
+import SwiftUI
+
+struct ImagePickerView: UIViewControllerRepresentable {
+    @Binding var isVisible:Bool
+    @Binding var image:Image?
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(isVisible: $isVisible, image: $image)
+    }
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let vc = UIImagePickerController()
+        vc.allowsEditing = true
+        vc.sourceType = .photoLibrary
+        
+        vc.delegate = context.coordinator
+        
+        return vc
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        
+    }
+    
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        @Binding var isVisible:Bool
+        @Binding var image:Image?
+        
+        init(isVisible: Binding<Bool>, image: Binding<Image?>) {
+            _isVisible = isVisible
+            _image = image
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            let uiimage = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+            image = Image(uiImage: uiimage)
+            isVisible = false
+        }
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            isVisible = false
+        }
+    }
+}
